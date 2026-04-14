8adbf6a386b47cee590de00c48ee5f8a44c058de
Freya Butterworth <freya.butterworth@skola.taby.se>
2026-04-14 08:58:35 +0200

    fucking nav

--- oolong.js
+++ oolong.js
@@ -1,1 +1,9 @@
-//holy moly js???
+function showSection(sectionId) {
+  const pages = document.querySelectorAll(".page");
+
+  pages.forEach(function(page) {
+    page.classList.remove("active");
+  });
+
+  document.getElementById(sectionId).classList.add("active");
+}

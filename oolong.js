d418aeb7ac3f1fe326aae2b7967145596628654a
Freya Butterworth <freya.butterworth@skola.taby.se>
2026-05-04 09:13:15 +0200

    vill ha bra betyg

---
+++ oolong.js
@@ -0,0 +1,14 @@
+function showSection(sectionId) {
+  const pages = document.querySelectorAll(".page");
+
+  pages.forEach(function(page) {
+    page.classList.remove("active");
+  });
+
+  document.getElementById(sectionId).classList.add("active");
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
